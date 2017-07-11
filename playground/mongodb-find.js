2909f91cfcbeeee9ab82771e57ab87594a7d84d6
Leonel <mml_leo@hotmail.com>
2017-07-11 14:08:41 -0300

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb') // sabe as up above
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('59645b0e973b1521792b5e29')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fech todos');
+    // })
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fech todos');
+    // })
+
+    db.collection('Users').find({
+        name: 'Leonel'
+    }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fech users');
+    })
+
+    // db.close()
+})
