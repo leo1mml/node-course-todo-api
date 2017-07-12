a1100f3094c4e65d7058abf311c1ff5b33aab49d
Leonel <mml_leo@hotmail.com>
2017-07-11 23:30:01 -0300

    inserting items in database

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose')
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+})
+
+module.exports = {
+    User
+}
+
+
+// var user = new User({
+//     email: 'mml_leo@hotmail.com'
+// })
+
+// user.save().then((doc) => {
+//     console.log("User saved", doc);
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// })
